26f22ddcdec32e6603031a4dde3e2659b2e6a5de
Diyar-Git <medetovdiyar4@gmail.com>
2025-05-04 19:33:14 +0500

    Delete others files.sh

--- simple-interest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter principal:"
-read p
-echo "Enter rate of interest:"
-read r
-echo "Enter time in years:"
-read t
-si=$(echo "scale=2; $p * $r * $t / 100" | bc)
-echo "Simple Interest is: $si"
